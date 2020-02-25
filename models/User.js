12c84a379a53a0e95f54bb2d0dc863e1282a4a7b
Alexorin <olexandrluy@gmail.com>
2020-02-25 10:47:44 +0200

    Request Validation

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    }
+    date: {
+        type: Data,
+        default: Data.now
+    }
+})
+
+module.exports = User = mongoose.model('user', userScheman)
